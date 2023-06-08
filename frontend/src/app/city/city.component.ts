272487cabf3855b0a9f07413de2a3a0875fbc824
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-06-08 11:28:21 +0530

    fetched drawn Zone Coordinates for city

--- frontend/src/app/city/city.component.ts
+++ frontend/src/app/city/city.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CityService } from '../Service/city.service';
 declare var google: any;
@@ -10,8 +10,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './city.component.html',
   styleUrls: ['./city.component.css']
 })
-export class CityComponent implements OnInit, AfterViewInit {
-  // cityData: any[] = []
+export class CityComponent implements OnInit {
   addedCities: string[] = [];
   newCity: string = '';
   isValidCity: boolean = false;
@@ -41,23 +40,10 @@ export class CityComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this.getCountryNamefromDB()
+    this.initMap();
   }
-
-  // TO fetch country data from from /countrydata API in dropdown..........
-  getCountryNamefromDB() :void{
-      this._city.getcountryData().subscribe({
-        next: (response) => {
-          this.countryData = response.countrydata;
-          // console.log(response.countrydata)
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      });
-    }
   
-
-  
+  // To set the Location of searched from Input field and modify it..........
   setLocation(place: any) {
     if (!place.geometry) {
       console.error('No geometry found for place:', place);
@@ -72,34 +58,27 @@ export class CityComponent implements OnInit, AfterViewInit {
     }
   }
 
-  ngAfterViewInit() {
+
+  // To load the map on Screen.............
+  initMap() {
     this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
       center: { lat: 20.5937, lng: 78.9629 },
       zoom: 4
     });
-    
+
+
+
     const input = document.getElementById('inputCity') as HTMLInputElement;
     this.autocomplete = new google.maps.places.Autocomplete(input);
 
     this.autocomplete.addListener('place_changed', () => {
       const place: any = this.autocomplete.getPlace();
+      this.setLocation(place)
 
-      if (!place.geometry) {
-        console.error('No geometry found for place:', place);
-        return;
-      }
-
-      // Move map marker to selected location
-      if (place.geometry && place.geometry.location) {
-        this.map.setCenter(place.geometry.location);
-        this.map.setZoom(12); // Increase the zoom level
-        this.marker.setPosition(place.geometry.location);
-        this.marker.setVisible(true);
-      }
     });
 
 
-    // Add Location Marker or Pin
+    // Add Location Marker or Pin on the searched location..............
     this.marker = new google.maps.Marker({
       map: this.map,
       draggable: true,
@@ -107,7 +86,7 @@ export class CityComponent implements OnInit, AfterViewInit {
       anchorPoint: new google.maps.Point(0, -29)
     });
 
-    // to draw the polygon on the map
+    // to draw the polygon on the map using DrawingManager()............
     this.drawingManager = new google.maps.drawing.DrawingManager({
       drawingMode: google.maps.drawing.OverlayType.POLYGON,
       drawingControl: true,
@@ -122,44 +101,64 @@ export class CityComponent implements OnInit, AfterViewInit {
     google.maps.event.addListener(this.drawingManager, 'overlaycomplete', (event: any) => {
       if (event.type === google.maps.drawing.OverlayType.POLYGON) {
         if (this.polygon) {
-          this.polygon.setMap(null);
+          this.polygon.setMap(null);  //clearing old drawn polygon
         }
-        // console.log(event);
-
         this.polygon = event.overlay;
-        // console.log(this.polygon);
       }
     });
   }
+  
+  // To fetch country data from from /countrydata API in dropdown..........
+  getCountryNamefromDB() :void{
+      this._city.getcountryData().subscribe({
+        next: (response) => {
+          this.countryData = response.countrydata;
+          // console.log(response.countrydata)
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  
 
+// To check the drawn Zone inside coordinates or not and add city in database.............
   checkZone_AddCity() {
     const geocoder = new google.maps.Geocoder();
     const input = document.getElementById('inputCity') as HTMLInputElement;
-    // console.log('.....checkLocation.......' + input.value);
 
     const payload = {
-      coordinates: this.coordinates,
-      city: this.inputValue,
-      countryid: this.country
+      country: this.country,
+      city: input.value,
+      coordinates: this.coordinates
     };
 
-    console.log(payload);
 
     geocoder.geocode({ address: input.value }, (results: any, status: any) => {
       if (status === 'OK') {
         const location = results[0].geometry.location;
+        // console.log(location)
         this.isInZone = google.maps.geometry.poly.containsLocation(location, this.polygon);
-        
-        if (this.isInZone) {
-          this._city.addcity(payload).subscribe({
-            next: (res: any) => {
-              this.citydatabasedata.push(res.city);
-              this.toastr.success(res.message);
-            },
-            error: (error) => {
-              this.toastr.warning(error.error.message);
-            },
-          })
+
+        if (this.isInZone == true) {
+          const polygonPath = this.polygon.getPath();
+          this.coordinates = polygonPath.getArray().map((results: { lat: () => any; lng: () => any; }) => ({
+            lat: results.lat(),
+            lng: results.lng(),
+          }));
+          console.log('Coordinates:', this.coordinates);
+          
+        this.toastr.success("City Added");
+        alert("coordinates fetched of zone")
+          // this._city.addcity(payload).subscribe({
+          //   next: (response: any) => {
+          //     this.citydatabasedata.push(response.city);
+          //     this.toastr.success(response.message);
+          //   },
+          //   error: (error) => {
+          //     this.toastr.warning(error.error.message);
+          //   },
+          // })
         } 
         else {
             this.toastr.warning("please choose city and create a zone");
@@ -172,9 +171,7 @@ export class CityComponent implements OnInit, AfterViewInit {
 
   // To select country selected value from dropdown to use it in city input...........
   onSelected(value: any) {
-    //   selected country id and api countrydata  match and when both id match then  that id  show a countryname that countryname to use in rest api
     this.country = value
-    
     this.countryData.map((country: any) => {
       if (country.countryName === value) {
         this.countryData = country.countryName
@@ -187,13 +184,14 @@ export class CityComponent implements OnInit, AfterViewInit {
         let rcountry = countryRes.filter((obj: any) => {
           return obj.name.common == this.countryData
         })
-
+        //getting country code like IN..............
         let code = rcountry[0].cca2.toLowerCase()
         this.autocomplete.setTypes(['(cities)']);
         this.autocomplete.setComponentRestrictions({ 'country': code });
       },
       error: (error: any) => {
         console.log("Country Selection Error....... ", error.message);
+        this.toastr.error(error.message)
       }
       }
     )
