0b50ef5ee6acebb726a8ea9ee8631764f3faa07e
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-06-11 23:59:53 +0530

    users component added

---
+++ backend/models/users.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+
+const user_Schema = new mongoose.Schema({
+  profile: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  useremail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  countrycode: {
+    type: String,
+    required: true,
+  },
+  userphone: {
+    type: String,
+    required: true,
+  }
+});
+
+const userModel = mongoose.model("userModel", user_Schema);  //mapping registration data into signup data
+
+
+
+module.exports = userModel;
