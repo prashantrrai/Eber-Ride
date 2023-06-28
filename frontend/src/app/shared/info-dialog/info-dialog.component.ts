6eb5046e447fa4984caae029dcd80860dec55d0d
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-06-28 12:29:20 +0530

    All Zones Shown on Country Select

---
+++ frontend/src/app/shared/info-dialog/info-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from "@angular/material/dialog";
+
+@Component({
+  selector: "app-info-dialog",
+  templateUrl: "./info-dialog.component.html",
+  styleUrls: ["./info-dialog.component.css"],
+})
+export class InfoDialogComponent implements OnInit{
+  dataArray: any[] = [];
+
+  constructor(
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<InfoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+  ngOnInit(): void {
+    this.dataArray = this.data;
+    console.log(this.dataArray)
+  }
+
+
+}
